e7d6e122b13c2baea5785a150af3f0431a62cc22
woongsnote <woongsnote@gmail.com>
2024-03-28 14:41:26 +0900

    feat: add rehype-pretty-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: false,
+      rehypePlugins: [
+        [
+          rehypePrettyCode,
+          {
+            theme: "one-dark-pro",
+          },
+        ],
+      ],
+    }),
+  ],
 });
